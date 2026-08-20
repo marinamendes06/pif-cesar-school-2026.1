433fbf8a3595471c42053d52c638241ef9c0852d
marinamendes06 <msm4@cesar.school>
2026-08-20 10:48:12 -0300

    Add basic program to display the current year

--- Lista-Cap1/exercicio01.c
+++ Lista-Cap1/exercicio01.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
+#include <locale.h>
 
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    int num = 2026;
+
+    printf("O ano que estamos é: %d", num);
+
+    return 0;
+}
