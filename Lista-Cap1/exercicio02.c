e25d7c7daa9cf38fbb28efa796f687f13f1b3986
marinamendes06 <msm4@cesar.school>
2026-08-20 11:01:57 -0300

    Create exercicio02.c

---
+++ Lista-Cap1/exercicio02.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    float num = 3.14159f;
+
+    printf("O número de π é: %.3f \n", num);
+
+    return 0;
+}
