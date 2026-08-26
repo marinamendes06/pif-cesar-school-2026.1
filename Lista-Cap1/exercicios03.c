2d39caa109a4564d751c9b36e8698ecff7393383
marinamendes06 <msm4@cesar.school>
2026-08-26 15:36:01 -0300

    Create exercicios03.c

---
+++ Lista-Cap1/exercicios03.c
@@ -0,0 +1,16 @@
+/* Esse programa mostra o uso de comentários em várias linhas
+* e mostra também o uso de comentários em uma única linha
+* 
+* Primeiro programa
+***************************************************************/
+/* Prog1.C */
+
+#include <stdio.h> /* Para printf() */
+#include <stdlib.h>/* Para system() */
+
+int main() /* Função main */
+{ /* início do corpo da função main */
+    printf("Primeiro programa."); /* Chamada à função printf */
+    system("PAUSE"); /* Chamada à função system */
+    return 0;
+}/* Fim do corpo da função main */
