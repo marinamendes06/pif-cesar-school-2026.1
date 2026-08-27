5d75069d717b3fd9a47dec70d0ee4649de55cd92
marinamendes06 <msm4@cesar.school>
2026-08-26 23:49:08 -0300

    Add exercicio22.c with ASCII art output

---
+++ Lista-Cap1/exercicio22.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("\xDC\xDC████\xDC\xDC\n");
+    printf("\xDFO\xDF\xDF\xDF\xDF\xDFO\xDF\n");
+  
+    printf("\n");
+
+    ▄▄█ ██████
+    printf("\xDC\xDC█ ██████\n");
+    printf("\xDFO\xDF\xDF\xDF\xDFOO\xDF\n");
+
+    return 0;
+}
