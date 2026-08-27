3bf96a9caacb727a56c55d022e41c964bde17f0d
marinamendes06 <msm4@cesar.school>
2026-08-26 23:51:09 -0300

    Add exercise23.c for printing a pattern

---
+++ Lista-Cap1/exercicio23.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("XXXXX\n");
+    
+    printf("X   X\n");
+    printf("X   X\n");
+    printf("X   X\n");
+    
+    printf("XXXXX\n");
+
+    return 0;
+}
