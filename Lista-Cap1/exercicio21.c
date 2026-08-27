7316045423cd6a000899a45f4beefd63cb58321a
marinamendes06 <msm4@cesar.school>
2026-08-26 23:46:44 -0300

    Add exercicio21.c with C programming examples

---
+++ Lista-Cap1/exercicio21.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    printf("Treinamento em programação.\nLinguagem C.\n");
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    printf("Treinamento em programação.\n");
+    printf("Linguagem C.\n");
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    printf("\xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBB\n");
+    
+    printf("\xBA%-28s\xBA\n", "Treinamento em programação.");
+    
+    printf("\xBA%-28s\xBA\n", "Linguagem C.");
+  
+    printf("\xC8\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBC\n");
+
+    return 0;
+}
