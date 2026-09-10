fd19cf8de5f608de430257b24ce4b6d440db746a
marinamendes06 <msm4@cesar.school>
2026-09-09 21:00:00 -0300

    Add basic program to read and display a character

--- Lista-Cap2/exercicio2.c
+++ Lista-Cap2/exercicio2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    char letra;
+
+    printf("Digite uma letra: ");
+    // Esse espaco antes do %c ignora o \n que sobrou
+    scanf(" %c", &letra); 
+
+    printf("Voce digitou: %c\n", letra);
+
+    return 0;
+}
